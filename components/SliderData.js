8e2c951f9ab4b16287070a2433ab84a29fd33e17
jr <changjiale@gmail.com>
2022-07-05 13:11:25 +0800

    updated

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,30 +1,31 @@
 export const SliderData = [
   {
-    image: '/../public/assets/movie1.png',
+    image: '/../public/static/movie1.png',
     description: "Landing page for a movie website",
   },
   {
     image:
-      '/../public/assets/movie2.png',
+      '/../public/static/movie2.png',
     description: "Search Results Page for a movie website",
   },
   {
     image:
-      '/../public/assets/movie3.png',
+      '/../public/static/movie3.png',
     description: "Individual Movie Page for a movie website",
   },
   {
     image:
-      '/../public/assets/adminProducts.png',
+      '/../public/static/adminProducts.png',
     description: "E-Commerce Admin Page",
   },
   {
     image:
-      '/../public/assets/sushi-main.png',
+      '/../public/static/sushi-main.png',
     description: "E-Commerce, Payment Page",
   },
   {
     image:
-      '/../public/assets/sushi.png', description: "E-Commerce, Landing Page"
+      '/../public/static/sushi.png',
+    description: "E-Commerce, Landing Page"
   },
 ];
